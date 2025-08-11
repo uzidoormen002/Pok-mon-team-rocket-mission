a03cb7dc3a0542172f4d3c80dc2fb7182e8e7175
uzidoormen002 <matthew.nath@sbhsstudent.school.nz>
2025-08-11 13:17:14 +1200

    gfhfghcnvhjg

--- team rocket mission/test.py
+++ team rocket mission/test.py
@@ -1,3 +1,9 @@
+import pygame
+import sys
+
+pygame.init()
+
+
 print("Hello I'm the narrator of the pokemon universe you have been chosen to redeem the failures that Jessie James and Meowth from a curtain point in time")
 print("sneak onto the island controled by Mewtwo to redeem them")
 print("to redeem them you must choose the actions Jessie James and Meowth take to try and steal all the pokemon on New island")
