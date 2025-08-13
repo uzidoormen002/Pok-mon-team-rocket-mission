0f83068d0a735383b3adc4c4d12bbd0786433b1f
uzidoormen002 <matthew.nath@sbhsstudent.school.nz>
2025-08-13 12:15:01 +1200

    vb   nnn

--- team rocket mission/test.py
+++ team rocket mission/test.py
@@ -3,7 +3,9 @@ import sys
 
 pygame.init()
 
-
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("team rockets biggest catch secret mission")
 
 ("Hello I'm the narrator of the pokemon universe you have been chosen to redeem the failures that Jessie James and Meowth from a curtain point in time sneak onto the island controled by Mewtwo to redeem them to redeem them you must choose the actions Jessie James and Meowth take to try and steal all the pokemon on New island top priority is capture Mewtwo if you can't then you can secondary objective is to capture other pokemon click begin when ready good luck hahahaha!!!")
 
