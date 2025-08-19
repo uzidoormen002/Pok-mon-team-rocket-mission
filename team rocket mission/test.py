337fbf96d517fc8924210b39b4ff6bbe23b2bdba
uzidoormen002 <matthew.nath@sbhsstudent.school.nz>
2025-08-19 14:58:37 +1200

    nmb,b

--- team rocket mission/test.py
+++ team rocket mission/test.py
@@ -7,54 +7,58 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("team rockets biggest catch secret mission")
 
+# Fonts
+font = pygame.font.SysFont("Arial", 24)
 
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-def main():
-    global current_level, game_state, selected_items
+# Characters
+team_rocket = ["Jessie", "James", "Meowth"]
+current_speaker = 0
 
-    clock = pygame.time.Clock()
+dialogue = [
+    ("Hello I'm the narrator of the pokemon universe you have been chosen"),
+    ("to redeem the past failures of Jessie James and Meowth from team rocket")
+    ("from a cutrain point of time you will have to chose the actions Jessie James and Meowth have to take to redeem themselfs"),
+    ("to do this you wil be sent to new island"),
+    ("Hey! What are you creeps doing here?!"),
+    ("We’re here to, uh, admire the Pokémon... totally not steal them!"),
+    ("Pika pii!"),
+    ("Even the Pikachu knows we’re lying..."),
+    ("You dare trespass on my island? Your ambition will be your downfall."),
+    ("Uh-oh..."),
+]
 
-    while True:
-        screen.fill(WHITE)
+dialogue_index = 0
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+# Game loop
+clock = pygame.time.Clock()
+running = True
 
-            if game_state == "level":
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    if current_level < total_levels:
-                        game_state = "item_select"
-                        selected_items = []
-                    else:
-                        print("Game Over!")
-                        pygame.quit()
-                        sys.exit()
+while running:
+    screen.fill(BLACK)
 
-            elif game_state == "item_select":
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mx, my = event.pos
-                    for rect, item in item_buttons:
-                        if rect.collidepoint(mx, my):
-                            if item in selected_items:
-                                selected_items.remove(item)
-                            elif len(selected_items) < 3:
-                                selected_items.append(item)
+    # Draw current dialogue
+    if dialogue_index < len(dialogue):
+        speaker, line = dialogue[dialogue_index]
+        text_surface = font.render(f"{speaker}: {line}", True, WHITE)
+        screen.blit(text_surface, (50, HEIGHT - 100))
+    else:
+        text_surface = font.render("End of Demo. To be continued...", True, WHITE)
+        screen.blit(text_surface, (50, HEIGHT - 100))
 
-                if len(selected_items) == 3:
-                    current_level += 1
-                    game_state = "level"
+    pygame.display.flip()
 
-        if game_state == "level":
-            draw_level_screen(current_level)
-        elif game_state == "item_select":
-            draw_item_selection()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dialogue_index += 1  # Move to next line of dialogue
 
-        pygame.display.flip()
-        clock.tick(60)
+    clock.tick(30)
 
-if __name__ == "__main__":
-    main()
-("Hello I'm the narrator of the pokemon universe you have been chosen to redeem the failures that Jessie James and Meowth from a curtain point in time sneak onto the island controled by Mewtwo to redeem them to redeem them you must choose the actions Jessie James and Meowth take to try and steal all the pokemon on New island top priority is capture Mewtwo if you can't then you can secondary objective is to capture other pokemon click begin when ready good luck hahahaha!!!")
-
+pygame.quit()
+sys.exit()
