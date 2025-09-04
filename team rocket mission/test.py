83e37b50fc38e3bf474e07da788f05b2e215757c
uzidoormen002 <matthew.nath@sbhsstudent.school.nz>
2025-09-05 10:01:26 +1200

    utdkd

--- team rocket mission/test.py
+++ team rocket mission/test.py
@@ -8,26 +8,25 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("team rockets biggest catch mission")
 
-
-text_surface = f.render("arial", True, WHITE)
-Background = "https://thf.bing.com/th/id/OIP.AMhKDordlB0tTufTM9q8DQHaER?w=294&h=180&c=7&r=0&o=7&cb=thfc1&pid=1.7&rm=3"
+font = pygame.font.SysFont("Arial", 24)
 
 WHITE = (210, 100, 0)
 BLACK = (10, 60, 160)
 yellow = (240, 217, 9)
+background_file = "title_screen.png"
+background = pygame.image.load("image/title_screen.png")   
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
-background = pygame.image.load
-background = pygame.transform.scale(Background, (WIDTH, HEIGHT))
 
-team_rocket = ["NARRATOR" "Jessie" "James" "Meowth"]
+team_rocket = ["NARRATOR", "Jessie", "James", "Meowth"]
 current_speaker = 0
 
 dialogue = [
     ("NARRATOR", "Hello I’m the narrator of the pokemon universe you have been chosen"),
     ("NARRATOR", "to live through the past failures of Jessie James and Meowth"),
-    ("NARRATOR", "from a cutrain point of time you will have to chose the actions of"),
-    ("NARRATOR", "Jessie James and Meowth to redeem themselfs"),
-    ("NARRATOR", "to do this you wil be sent to new island"),
+    ("NARRATOR", "from a cetrain point of time you will have to chose the actions of"),
+    ("NARRATOR", "Jessie James and Meowth to redeem themselves"),
+    ("NARRATOR", "to do this you will be sent to new island"),
     ("NARRATOR", "your mission is to capture all the pokemon on the island"),
     ("NARRATOR", "or at least some of the pokemon on the island you choose which"),
     ("NARRATOR", "and if you can capture Mewtwo but it will be difficult to do"),
@@ -46,7 +45,7 @@ running = True
 while running:
     
     screen.blit(background, (0, 0))
-
+    50, HEIGHT - 120
 
     if dialogue_index < len(dialogue):
         speaker, line = dialogue[dialogue_index]
@@ -54,7 +53,7 @@ while running:
         screen.blit(text_surface, (50, HEIGHT - 100))
     else:
         text_surface = font.render("you have failed, better luck next time or not HAHAHAHA!!!", True, WHITE)
-        screen.blit(text_surface, (50, HEIGHT - 100))
+        screen.blit(text_surface, (50, HEIGHT - 120))
 
 
 
